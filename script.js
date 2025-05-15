e5377b2995ffc723a39baed2de88a2a9cc7a971d
pladde <mpoerschke@proton.me>
2025-05-15 10:41:23 +0200

    function/the data format is  now checked and will be transmitted

--- script.js
+++ script.js
@@ -1,13 +1,51 @@
-let dataName = document.getElementsByName("file");
+let dataContent;
+
+function saveData(files) {
+    dataContent = files;
+    validateDataFormat();
+}
 
 // Prüft ob es sich um ein erlaubes Datenformat handelt
-function validateDragAndDrop(dataName) {
+function validateDataFormat() {
 
-    let allowedFormats = ['csv', 'json', 'xml'];
+    let foundDataName = dataContent[0].name;
+    console.log("found file: " + foundDataName);
+    let formatIsAllowed = false;
 
-    for(let i = 0; i <= allowedFormats.length; i++) {
-        if(dataName.contains(allowedFormats[i]) === false) {
-            alert("please choose an allowed data format!");
+    let validFormats = ['.json', '.xml','.csv'];
+    console.log("valid formats: " + validFormats);
+
+    console.log("array length: " + validFormats.length);
+
+    for(let i = 0; i < validFormats.length; i++) {
+
+        console.log("while go through: " + i);
+        if(foundDataName.includes(validFormats[i])) {
+            console.log("found valid format: " + validFormats[i])
+            formatIsAllowed = true;
         }
     }
+
+    if(formatIsAllowed === false) {
+        console.log("no valid format found");
+        alert("Please choose a valid data-format!")
+    }
+
+    return formatIsAllowed;
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+    let sendButton = document.getElementById("send");
+
+    sendButton.addEventListener("click", function () {
+        // Code zum übermitteln hier
+
+        console.log("CLICK/found data name: " + dataContent.name);
+        if(validateDataFormat(dataContent.name)) {
+            alert("Click successfully!");
+        }
+        else {
+            alert("Please insert a valid data-format and try again!");
+        }
+    });
+});
